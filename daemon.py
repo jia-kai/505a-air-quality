3a76fad48fdb9a5c8755e5a8b8ade14bad62968c
Jia Kai <jia.kai66@gmail.com>
2014-03-02 16:14:16 +0800

    daemon

---
+++ daemon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# $File: daemon.py
+# $Date: Sun Mar 02 16:07:38 2014 +0800
+# $Author: jiakai <jia.kai66@gmail.com>
+
+from sample_and_record import insert_db_entry
+
+import time
+
+SAMPLE_DELTA = 300
+
+if __name__ == '__main__':
+    while True:
+        insert_db_entry()
+        time.sleep(SAMPLE_DELTA)
+
