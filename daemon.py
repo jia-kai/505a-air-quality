a01667353122dd45ec90a63e1a170f56f32dfa67
Jia Kai <jia.kai66@gmail.com>
2014-03-03 09:36:47 +0800

    do not sample within 2 minutes after boot

--- daemon.py
+++ daemon.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: daemon.py
-# $Date: Sun Mar 02 20:25:35 2014 +0800
+# $Date: Mon Mar 03 09:35:26 2014 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
 
 from sample_and_record import insert_db_entry
 
 import time
 
+MIN_UPTIME = 120
 SAMPLE_DELTA = 300
 
+def get_uptime():
+    with open('/proc/uptime') as fin:
+        return map(float, fin.read().split())[0]
+
 if __name__ == '__main__':
+    if get_uptime() < MIN_UPTIME:
+        time.sleep(MIN_UPTIME)
     while True:
         insert_db_entry()
         time.sleep(SAMPLE_DELTA)
